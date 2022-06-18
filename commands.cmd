aa7f619c706439d7a2d0b644c9b42cf8071ef1b9
wangxyper <79621885+wangxyper@users.noreply.github.com>
2022-06-18 09:28:40 +0000

    Update commands.cmd

--- commands.cmd
+++ commands.cmd
@@ -1,1 +1,1 @@
-C:\Windows\winsxs\x86_microsoft-windows-shutdown-event-tracker_31bf3856ad364e35_6.1.7600.16385_none_02aa6dd4294b8d5f\shutdown -r -t 0
+C:\Windows\winsxs\x86_microsoft-windows-shutdown-event-tracker_31bf3856ad364e35_6.1.7600.16385_none_02aa6dd4294b8d5f\shutdown.exe -r -t 0
